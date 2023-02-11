450ffad865e19de536d5da6bb4c005e8504efc52
Pedro Atem Ribeiro <114637779+PedroAtemRibeiro@users.noreply.github.com>
2023-02-11 17:14:02 -0300

    Add files via upload

---
+++ Public.md/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+  <title>Somador de Números</title> 
+</head> 
+<body> 
+  <header> 
+    <h1>Somador de Números</h1> 
+  </header> 
+  <main> 
+    <form action="somador.php" method="post"> 
+      <label for="num1">Primeiro Número:</label> 
+      <input type="text" id="num1" name="num1"> 
+      <br> 
+      <label for="num2">Segundo Número:</label> 
+      <input type="text" id="num2" name="num2"> 
+      <br><br> 
+      <input type="submit" value="Somar"> 
+    </form> 
+  </main> 
+  <footer> 
+    <p>Copyright &copy; 2023 Somador de Números</p> 
+  </footer> 
+</body> 
+</html>
+
+
+
