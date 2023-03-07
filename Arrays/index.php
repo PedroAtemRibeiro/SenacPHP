3d9b64be65a07d20872b85ce27a864bbd3f2debb
Pedro Atem Ribeiro <114637779+PedroAtemRibeiro@users.noreply.github.com>
2023-03-07 17:16:23 -0300

    CommitCapitulo1
    Aulas Capítulo1

---
+++ Arrays/index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Lista de Nomes</title>
+  </head>
+  <body>
+    <?php
+      require 'lista.php'
+    ?>
+  </body>
+</html>
---
+++ Get-Post/post.php
@@ -0,0 +1,5 @@
+<?php
+  if(isset($_POST['mood'])){
+   echo '<h2>Você está ' . $_POST['mood'] . '<h2>';
+  }
+?> 
