d9a78e4fb5520f0c3875a64ad877b3d791818ed9
Pedro Atem Ribeiro <114637779+PedroAtemRibeiro@users.noreply.github.com>
2023-03-07 17:38:47 -0300

    Delete Forms-ArraysPHP directory

--- Forms-ArraysPHP/get.php
+++
@@ -1,8 +0,0 @@
-<?php	$sabores	=	['Mussarela',	'Palmito',	'Calabresa'];?>
-
-<?php
-  if(array_key_exists('sabores', $_GET)){
-    foreach($_GET['sabores'] as $sabor_escolhido){
-      echo 'Você escolheu o sabor ' . $sabor_escolhido . '<br>';
-   }
-}?>
